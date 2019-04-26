7d725e5f9bd43afb2f090cd8c77b8ebac13d799d
syn <vicdemilly@gmail.com>
2019-04-26 13:54:55 +0200

    Réalisation d'un formulaire renvoyons une appréciation selon la moyenne calculée

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+  var note1 = Number($('#note1').val());
+  var note2 = Number($('#note2').val());
+  var note3 = Number($('#note3').val());
+  var note4 = Number($('#note4').val());
+  var note5 = Number($('#note5').val());
+  $('#submit').on('click', function(){
+    console.log(note1);
+    console.log(note2);
+    console.log(note3);
+    console.log(note4);
+    console.log(note5);
+    var moyenne = (note1 + note2 + note3 + note4 + note5) / 5;
+    if (moyenne < 10){
+      alert('Vous avez ' + moyenne + ' de moyenne. Gros naze.');
+    }
+    if (moyenne < 13 && moyenne >= 10) {
+      alert('Vous avez ' + moyenne + ' de moyenne. Feignasse.');
+    }
+    if (moyenne < 16 && moyenne >= 13){
+      alert('Vous avez ' + moyenne + ' de moyenne. Beau gosse.');
+    }
+    if (moyenne < 20 && moyenne >= 16){
+      alert('Vous avez ' + moyenne + ' de moyenne. Lèche-cul.');
+    }
+    if (moyenne == 20){
+      alert('Vous avez ' + moyenne + ' de moyenne. Putain de no-life.');
+    }
+    else {
+      alert('Attention, certains de vos champs ne sont pas valides. Veuillez indiquer un nombre entre 0 et 20.');
+      $('.notes').css('border', 'solid red 3px');
+    }
+  });
+});
